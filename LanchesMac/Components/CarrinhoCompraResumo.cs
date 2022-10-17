671bb0acdec2984d2f81f9548b22fdf8501213fe
Fabio Luiz <fbluiz.sf@gmail.com>
2022-10-17 02:24:29 -0300

    botao com metodo pra adicionar ao carrinho

--- LanchesMac/Components/CarrinhoCompraResumo.cs
+++ LanchesMac/Components/CarrinhoCompraResumo.cs
@@ -15,13 +15,13 @@ namespace LanchesMac.Components
 
         public IViewComponentResult Invoke()
         {
-            //var itens = _carrinhoCompra.GetCarrinhoCompraItens();
+            var itens = _carrinhoCompra.GetCarrinhoCompraItens();
 
-            var itens = new List<CarrinhoCompraItem>()
-            {
-                new CarrinhoCompraItem(),
-                new CarrinhoCompraItem()
-            };
+            //var itens = new List<CarrinhoCompraItem>()
+            //{
+            //    new CarrinhoCompraItem(),
+            //    new CarrinhoCompraItem()
+            //};
 
             _carrinhoCompra.CarrinhoCompraItems = itens;
 
--- LanchesMac/Controllers/LancheController.cs
+++ LanchesMac/Controllers/LancheController.cs
@@ -2,14 +2,12 @@
 using LanchesMac.Repositories.Interfaces;
 using LanchesMac.ViewModels;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
 
 namespace LanchesMac.Controllers
 {
     public class LancheController : Controller
     {
         private readonly ILancheRepository _lancheRepository;
-
         public LancheController(ILancheRepository lancheRepository)
         {
             _lancheRepository = lancheRepository;
@@ -23,18 +21,42 @@ namespace LanchesMac.Controllers
             if (string.IsNullOrEmpty(categoria))
             {
                 lanches = _lancheRepository.Lanches.OrderBy(l => l.LancheId);
-                categoriaAtual = "Todos";
+                categoriaAtual = "Todos os lanches";
             }
             else
             {
+                //if (string.Equals("Normal", categoria, StringComparison.OrdinalIgnoreCase))
+                //{
+                //    lanches = _lancheRepository.Lanches
+                //        .Where(l => l.Categoria.CategoriaNome.Equals("Normal"))
+                //        .OrderBy(l => l.Nome);
+                //}
+                //else
+                //{
+                //    lanches = _lancheRepository.Lanches
+                //       .Where(l => l.Categoria.CategoriaNome.Equals("Natural"))
+                //       .OrderBy(l => l.Nome);
+                //}
                 lanches = _lancheRepository.Lanches
-                    .Where(c => c.Categoria.CategoriaNome.Equals(categoria))
-                    .OrderBy(c => c.Nome);
+                          .Where(l => l.Categoria.CategoriaNome.Equals(categoria))
+                          .OrderBy(c => c.Nome);
+
                 categoriaAtual = categoria;
             }
-            var lanchesListViewModel = new LancheListViewModel { Lanches = lanches, CategoriaAtual = categoriaAtual };
+
+            var lanchesListViewModel = new LancheListViewModel
+            {
+                Lanches = lanches,
+                CategoriaAtual = categoriaAtual
+            };
 
             return View(lanchesListViewModel);
+        }
+
+        public IActionResult Details(int lancheId)
+        {
+            var lanche = _lancheRepository.Lanches.FirstOrDefault(l => l.LancheId == lancheId);
+            return View(lanche);
         }
     }
 }
